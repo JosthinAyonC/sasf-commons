7a2c18ee914a9f348f85ee7eea153bba8c43bb97
Chustin <josthinayon@gmail.com>
2025-02-03 15:58:19 -0500

    feat: dialog & table changes

--- components/ui/Table.tsx
+++ components/ui/Table.tsx
@@ -4,12 +4,16 @@ import React from 'react';
 interface TableProps<T extends object> {
   data: T[];
   columns: ColumnDef<T>[];
+  searchable?: boolean;
+  showOptions?: boolean;
+  defaultPage?: number;
+  defaultSize?: number;
 }
 
-const Table = <T extends object>({ data, columns }: TableProps<T>) => {
+const Table = <T extends object>({ data, columns, defaultPage = 0, defaultSize = 5, showOptions = true, searchable = true }: TableProps<T>) => {
   const [pagination, setPagination] = React.useState({
-    pageIndex: 0,
-    pageSize: 5,
+    pageIndex: defaultPage,
+    pageSize: defaultSize,
   });
 
   const [globalFilter, setGlobalFilter] = React.useState('');
@@ -31,14 +35,16 @@ const Table = <T extends object>({ data, columns }: TableProps<T>) => {
   return (
     <div className="flex flex-col ">
       {/* Campo de búsqueda */}
-      <input
-        type="text"
-        value={globalFilter ?? ''}
-        onChange={(e) => setGlobalFilter(e.target.value)}
-        placeholder="Buscar..."
-        className="mb-4 p-2 border-[var(--border)] rounded w-full bg-[var(--bg)] text-[var(--font)] placeholder-[var(--placeholder)]
+      {searchable && (
+        <input
+          type="text"
+          value={globalFilter ?? ''}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Buscar..."
+          className="mb-4 p-2 border-[var(--border)] rounded w-full bg-[var(--bg)] text-[var(--font)] placeholder-[var(--placeholder)]
                   focus:outline-none focus:ring focus:border-[var(--highlight)]"
-      />
+        />
+      )}
 
       <div className="overflow-x-auto rounded-xl">
         <div className="inline-block min-w-full py-2">
@@ -68,57 +74,59 @@ const Table = <T extends object>({ data, columns }: TableProps<T>) => {
               </tbody>
             </table>
             {/* Controles de paginación */}
-            <div className="flex items-center justify-between mt-4">
-              <div className="flex items-center space-x-2">
-                <button
-                  onClick={() => table.setPageIndex(0)}
-                  disabled={!table.getCanPreviousPage()}
-                  className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded disabled:opacity-50"
+            {showOptions && (
+              <div className="flex items-center justify-between mt-4">
+                <div className="flex items-center space-x-2">
+                  <button
+                    onClick={() => table.setPageIndex(0)}
+                    disabled={!table.getCanPreviousPage()}
+                    className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded disabled:opacity-50"
+                  >
+                    {'<<'}
+                  </button>
+                  <button
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                    className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded disabled:opacity-50"
+                  >
+                    {'<'}
+                  </button>
+                  <button
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                    className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded disabled:opacity-50"
+                  >
+                    {'>'}
+                  </button>
+                  <button
+                    onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                    disabled={!table.getCanNextPage()}
+                    className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded disabled:opacity-50"
+                  >
+                    {'>>'}
+                  </button>
+                </div>
+                <span className="text-[var(--font)]">
+                  Página{' '}
+                  <strong>
+                    {table.getState().pagination.pageIndex + 1} de {table.getPageCount()}
+                  </strong>
+                </span>
+                <select
+                  value={table.getState().pagination.pageSize}
+                  onChange={(e) => {
+                    table.setPageSize(Number(e.target.value));
+                  }}
+                  className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded"
                 >
-                  {'<<'}
-                </button>
-                <button
-                  onClick={() => table.previousPage()}
-                  disabled={!table.getCanPreviousPage()}
-                  className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded disabled:opacity-50"
-                >
-                  {'<'}
-                </button>
-                <button
-                  onClick={() => table.nextPage()}
-                  disabled={!table.getCanNextPage()}
-                  className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded disabled:opacity-50"
-                >
-                  {'>'}
-                </button>
-                <button
-                  onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-                  disabled={!table.getCanNextPage()}
-                  className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded disabled:opacity-50"
-                >
-                  {'>>'}
-                </button>
+                  {[5, 10, 20, 30, 40, 50].map((pageSize) => (
+                    <option key={pageSize} value={pageSize}>
+                      Mostrar {pageSize}
+                    </option>
+                  ))}
+                </select>
               </div>
-              <span className="text-[var(--font)]">
-                Página{' '}
-                <strong>
-                  {table.getState().pagination.pageIndex + 1} de {table.getPageCount()}
-                </strong>
-              </span>
-              <select
-                value={table.getState().pagination.pageSize}
-                onChange={(e) => {
-                  table.setPageSize(Number(e.target.value));
-                }}
-                className="px-3 py-1 bg-[var(--hover)] text-[var(--font)] rounded"
-              >
-                {[5, 10, 20, 30, 40, 50].map((pageSize) => (
-                  <option key={pageSize} value={pageSize}>
-                    Mostrar {pageSize}
-                  </option>
-                ))}
-              </select>
-            </div>
+            )}
           </div>
         </div>
       </div>
