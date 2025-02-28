d3a50a7505aa1e49f8101ee338e228c3dca64c34
Chustin <josthinayon@gmail.com>
2025-02-28 12:17:59 -0500

    style: lint

--- config/eventEmitter.ts
+++ config/eventEmitter.ts
@@ -1,4 +1,4 @@
-import EventEmitter from "eventemitter3";
+import EventEmitter from 'eventemitter3';
 
 // Evento para poder comunicarle algo a Query Table desde modales
 export const tableEventEmitter = new EventEmitter();
