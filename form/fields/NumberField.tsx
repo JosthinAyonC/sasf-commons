14e7eea4686ca9c291ccc87a6f0c3768bbdd1e94
PREPASANTE TEMPORAL 1 <temporal@sasf.local>
2025-03-06 17:25:50 -0500

    feat: correccion en formato required

--- form/fields/NumberField.tsx
+++ form/fields/NumberField.tsx
@@ -76,9 +76,9 @@ export const NumberField = <T extends FieldValues>({
         disabled={disabled}
         {...register(name, {
           onChange: handleInput,
-          required: isRequired ? requiredMsg || 'Este campo es obligatorio' : undefined,
-          min: min ? { value: min, message: `El valor mínimo es ${min}` } : undefined,
-          max: max ? { value: max, message: `El valor máximo es ${max}` } : undefined,
+          required: isRequired ? requiredMsg || ' Este campo es obligatorio' : undefined,
+          min: min ? { value: min, message: ` El valor mínimo es ${min}` } : undefined,
+          max: max ? { value: max, message: ` El valor máximo es ${max}` } : undefined,
         })}
         className={`border border-[var(--border)] rounded-md p-2 w-full 
             focus:outline-none focus:border-[var(--focus)] placeholder:text-[var(--placeholder)] 
